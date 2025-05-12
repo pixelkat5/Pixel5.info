608aa5f4141f76e4a47641c3f8c052aa457688a1
Pixel <pixelsworkemail@gmail.com>
2025-05-12 14:55:55 +0000

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById("yes").addEventListener("click", () => {
+  alert("You clicked Yes!");
+  // Add any further action for 'Yes' here
+});
+
+document.getElementById("no").addEventListener("click", () => {
+  alert("You clicked No!");
+  // Add any further action for 'No' here
+});
